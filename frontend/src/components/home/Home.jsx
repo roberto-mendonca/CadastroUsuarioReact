583fa8e265498e9e5f8756f4614ef511ca3dbed3
Roberto Mendonça <robertomendonca20@gmail.com>
2019-02-14 22:06:28 -0200

    commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from '../template/Main'
+
+
+export default props => 
+    <Main icon="home" title="Início"
+        subtitle="Projeto Cadastro de Usuários">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema básico para cadastro de usuários em <strong>React!</strong></p>
+    </Main>
+
+
+
+
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong><a href="https://github.com/roberto-mendonca"> Roberto Mendonça </a><i class="fa fa-rocket"></i></strong> 
+        </span>
+    </footer>
